0ef4f480ef0504104a384cee4d9f7a000098b84f
JC Balcita <jc.balcita@hulu.com>
2019-08-29 15:20:37 -0700

    Use pattern matching

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,10 +1,11 @@
 pub fn is_leap_year(year: u64) -> bool {
-    if year % 4 == 0 {
-        if year % 100 == 0 {
-            return year % 400 == 0
-        }
-        return true
-    }
+    let divisble_by_four = year % 4 == 0;
+    let divisible_by_hundred = year % 100 == 0;
+    let divisble_by_four_hundred = year % 400 == 0;
 
-    return false
+    match (divisble_by_four, divisible_by_hundred, divisble_by_four_hundred) {
+        (true, true, b) => b,
+        (true, false, _) => true,
+        _ => false
+    }
 }
