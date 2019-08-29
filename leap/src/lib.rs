4bbd23128e9e9c8b5aec5d29902fcff480005dab
JC Balcita <jc.balcita@hulu.com>
2019-08-29 15:24:14 -0700

    Simplify for final answer

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,11 +1,3 @@
 pub fn is_leap_year(year: u64) -> bool {
-    let divisble_by_four = year % 4 == 0;
-    let divisible_by_hundred = year % 100 == 0;
-    let divisble_by_four_hundred = year % 400 == 0;
-
-    match (divisble_by_four, divisible_by_hundred, divisble_by_four_hundred) {
-        (true, true, b) => b,
-        (true, false, _) => true,
-        _ => false
-    }
+    year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
 }
